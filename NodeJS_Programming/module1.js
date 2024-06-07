e9844a7b9bbaaf3f666d4400648b82406373f996
AksarPankajKiyoo <aksarpankajkiyoo@gmail.com>
2024-06-07 09:17:00 +0530

    Sending NodeJS Dir & DateTime.exe file

---
+++ NodeJS_Programming/module1.js
@@ -0,0 +1,11 @@
+// function multi(a, b){
+//     return a*b;
+// }
+
+// function div(a,b){
+//     return a/b;
+// }
+
+// module.exports = {multi, div};
+exports.add = (a,b) => (a+b);
+exports.sub = (a,b) => (a-b);
---
+++ NodeJS_Programming/welcomeMsg.js
@@ -0,0 +1,5 @@
+console.log("Welcome to NodeJS Coding");
+// console.log(alert("hello"));
+const math = require('./module1');
+
+console.log(math.add(3,4));
